eb267e4485010a303aef227991a06380802776b9
marzubiri <melezubiri@gmail.com>
2026-02-12 12:17:25 -0300

    fix: modificacion constante IP
    unificación de IP para no hacer cambio constante en producción

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ BigInt.prototype.toJSON = function () { return this.toString(); };
 const app = require('./app');
 const prisma = require('./config/prisma');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3301;
 
 app.listen(PORT, () => {
   console.log(`Servidor backend corriendo en puerto ${PORT}`);
