f6d8da7f588815e0bd1aea826b32310666892a22
marzubiri <melezubiri@gmail.com>
2026-02-16 18:16:20 -0300

    feat: pantalla 'Mis reseñas'
    Lógica de muestra de listado de reseñas

--- backend/src/routes/resena.routes.js
+++ backend/src/routes/resena.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { crearResenaController } = require('../controllers/resena.controller');
+const { crearResenaController, getMisResenasController } = require('../controllers/resena.controller');
 
 const router = express.Router();
 
 router.post('/api/resenas', crearResenaController);
+router.get('/api/resenas/mias', getMisResenasController);
 
 module.exports = router;
