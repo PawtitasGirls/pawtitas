c04c6c416e7c05ccd470dfb174fad24b0dbbdcc8
majosetoro <toromajose@gmail.com>
2026-02-20 17:00:59 -0300

    fix(oauth): usar prestadorId en lugar de userId al conectar MercadoPago en Home.js

--- src/main-app/screens/home/home.js
+++ src/main-app/screens/home/home.js
@@ -292,7 +292,7 @@ const HomeScreen = () => {
         )}
 
         {isPrestadorActivo && (
-          <MercadoPagoConnect prestadorId={user?.id} />
+          <MercadoPagoConnect prestadorId={user?.prestadorId ?? user?.id} />
         )}
       </ScrollView>
 
